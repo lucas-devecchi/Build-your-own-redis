fd2e474cfdf61659a58d1e51f0dd84420c3b200e
lucas-devecchi <nahueldevecchi@gmail.com>
2026-01-21 17:00:49 +0000

    feat: copy int main from U6 in U7

--- 07/server.cpp
+++ 07/server.cpp
@@ -135,6 +135,7 @@ static void make_response(const Response &resp, vector<uint8_t> &out)
 
 int main()
 {
+    // the listening socket
     int listeningFd = socket(AF_INET, SOCK_STREAM, 0);
     if (listeningFd < 0)
         die("socket()");
@@ -151,5 +152,88 @@ int main()
     if (rv)
         die("bind()");
 
+    fd_set_nb(listeningFd);
+
+    // listen
+    rv = listen(listeningFd, SOMAXCONN);
+    if (rv)
+        die("listen()");
+
+    // a map of all client connections, indexed by fd
+    vector<Conn *> fd2conn;
+
+    // The event loop
+    vector<struct pollfd> poll_args;
+    while (true)
+    {
+        // prepare the arguments of the poll()
+        poll_args.clear();
+
+        // put the listening sockets in the first position
+        struct pollfd pfd = {listeningFd, POLLIN, 0};
+        poll_args.push_back(pfd);
+
+        // the rest are connection sockets
+        for (Conn *conn : fd2conn)
+        {
+            if (!conn)
+                continue;
+
+            struct pollfd pfd = {conn->fd, POLLERR, 0};
+
+            // poll() flags from the application's intent
+
+            /*
+                |= is a Logic OR, 0100 | 0001 equals 0101
+                Can keep the POLLIN when assign POLLOUT.
+                With only =, POLLIN would be removed (if it was assigned before).
+            */
+            if (conn->want_read)
+                pfd.events |= POLLIN;
+
+            if (conn->want_write)
+                pfd.events |= POLLOUT;
+
+            poll_args.push_back(pfd);
+        }
+
+        // wait for readinesss
+        int rv = poll(poll_args.data(), (nfds_t)poll_args.size(), -1);
+        if (rv < 0)
+        {
+            if (errno == EINTR) // when syscall is aborted by Unix, it returns EINTR. must iterate.
+                continue;
+            die("poll");
+        }
+
+        // handle the listening socket
+        if (poll_args[0].revents)
+        {
+            if (Conn *conn = handle_accept(listeningFd))
+            {
+                // put it into the map (resize if needed)
+                if (fd2conn.size() <= (size_t)conn->fd)
+                    fd2conn.resize(conn->fd + 1);
+                fd2conn[conn->fd] = conn;
+            }
+        }
+
+        for (size_t i = 1; i < poll_args.size(); i++)
+        {
+            uint32_t ready = poll_args[i].revents;
+            Conn *conn = fd2conn[poll_args[i].fd];
+            if (ready & POLLIN)
+                handle_read(conn);
+            if (ready & POLLOUT)
+                handle_write(conn);
+
+            if ((ready & POLLERR) || conn->want_close)
+            {
+                (void)close(conn->fd);
+                fd2conn[conn->fd] = NULL;
+                delete conn;
+            }
+        }
+    }
     return 0;
 }
